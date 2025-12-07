4f95e7fc38f7483afb4cf0ef82e0267a2eb4c75c
disasterdevy <36893414+disasterdevy@users.noreply.github.com.>
2025-12-07 13:41:24 +0100

    Add solution for multiply, power and factorial.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -6,26 +6,44 @@ const subtract = function(first, second) {
 	return first - second;
 };
 
-
+// array.reduce() must be returned otherwise no value is returned
 const sum = function(array) {
-  const initialValue = 0;
-  return array.reduce((total, currentValue) => {
-    return total + currentValue;
-  }, initialValue);
+  return array.reduce((accumulator, array_value) => {
+    return accumulator + array_value;
+  }, 0)
 };
 
 
-const multiply = function() {
- 
+const multiply = function(array) {
+   return array.reduce((accumulator, array_value) => {
+    return accumulator * array_value;
+  }, 1)
 };
 
 const power = function(base, exponent) {
-	 return base * exponent;
+	 return base ** exponent;
 };
 
-const factorial = function(first, second) {
-	
+const factorial = function(value) {
+  let total = value;
+  let second_value = total - 1; 
+  for(let i=value; i > 1; i--){
+      total = total * second_value;
+      second_value = second_value - 1;
+      console.log(total);
+    }
+  // exception for zero
+  if(total == 0){
+    total += 1;
+  }
+  
+  return total;
 };
+
+//   const initialValue = 0;
+//  return array.reduce((total, currentValue) => {
+//    return total + currentValue;
+//  }, initialValue);
 
 // Do not edit below this line
 module.exports = {
