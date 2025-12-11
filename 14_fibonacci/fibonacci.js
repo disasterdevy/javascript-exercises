13be60f129b48b65293cbbddf97c5317c337df93
disasterdevy <36893414+disasterdevy@users.noreply.github.com.>
2025-12-11 11:06:51 +0100

    Create solution for fibonacci. One test not successfull: 1st fibonacci is 1.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let num_one = 0
+    let num_two = 1
+    let fib = 0
+    // converting n to an integer
+    n_num = Number(n);
 
+    if(n_num >= 0){
+        for(n_num;n_num > 1; n_num--){
+            fib = num_one + num_two;
+            num_one = num_two;
+            num_two = fib;
+        };
+        return fib;
+    }
+    else
+        return "OOPS";
 };
 
 // Do not edit below this line
