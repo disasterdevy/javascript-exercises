1bf5d9f61bd528e3e0021c3514ba5e805e479333
disasterdevy <36893414+disasterdevy@users.noreply.github.com.>
2025-12-11 10:43:51 +0100

    Add solution for palindromes exercise.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // a good way to get rid off unnecessary 
+    const clean_string = string.toLowerCase().replace(/[^a-z0-9]/gi, "");
+    const reverse = clean_string.split('').reverse().join('');
+    return clean_string === reverse;
 };
 
 // Do not edit below this line
