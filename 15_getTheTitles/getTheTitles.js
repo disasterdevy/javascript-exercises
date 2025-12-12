4ad14123d53331acc8284ad059fd48926be0917f
disasterdevy <36893414+disasterdevy@users.noreply.github.com.>
2025-12-12 06:31:27 +0100

    Add beginning of solution. Have to find a way how to access obj through funciton.

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    
+    const results = []
+    // how do I access properties of an object through a function? 
+    for(obj in array){
+        return obj[1]
+    }
+        
 };
 
 // Do not edit below this line
