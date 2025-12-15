8142326093e577fe4086cc032caf788180424198
disasterdevy <36893414+disasterdevy@users.noreply.github.com.>
2025-12-15 06:40:54 +0100

    Create function getAge() to calculate age separatley. Fix findTheOldest() so it calculates the given iteratives accordingly and returns solution.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = function(birth, death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((current, next) => {
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        const nextAge = getAge(next.yearOfBirth, next.yearOfDeath);
+        return currentAge < nextAge ? next : current;
+    });
 };
 
 // Do not edit below this line
